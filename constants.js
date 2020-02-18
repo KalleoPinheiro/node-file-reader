ebf264d7bb281ef4f9b7c03e9059be2cdc1bb9d6
Kalléo Pinheiro <kalleopinheiro@hotmail.com>
2020-02-18 16:02:24 -0300

    Include register change to rules

---
+++ constants.js
@@ -0,0 +1,37 @@
+const columns = {
+  adhesion: {
+    city_code: 27,
+    uf_code: 29,
+    customer: 1,
+    uf: 30,
+    payment: 62,
+    ocs_activation: 40,
+    description: 41
+  },
+  purchase: {
+    customer: 1,
+    payment: 28,
+    ocs_activation: 12,
+    description: 13
+  },
+  registerChange: {
+    customer: 1,
+    uf: 19,
+    city_code: 16,
+    uf_code: 18
+  }
+}
+
+const kind = {
+  adhesion: 'adhesion',
+  purchase: 'purchase',
+  registerChange: 'registerChange'
+}
+
+const regex = {
+  adhesion: /ADESAO_PRIMEIRA_COMPRA/g,
+  purchase: /COMPRA_SERVICO/g,
+  registerChange: /ALTERACAO_CADASTRAL/g
+}
+
+module.exports = { columns, regex, kind }
